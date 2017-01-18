3df248d2560987c31f2c83741c2434c6f4e8f7e1
akshay <sharmaakshay390@gmail.com>
2017-01-18 16:27:49 +0530

    First try in completing Pro assign 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the inverse of the matrix
 
-## Write a short comment describing this function
+## Function to create a special matrix object which can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## Function to compute the inverse of the special matrix object returned by makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv_mat <- x$getinverse()
+  if(!is.null(inv_mat))
+  {
+    message('getting cached data')
+    return(inv_mat)
+  }
+  mat <- x$get()
+  inv_mat <- solve(mat)
+  x$setinverse(inv_mat)
+  inv_mat
 }
